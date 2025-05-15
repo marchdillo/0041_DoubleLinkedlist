9de2ca2d0331feccb93c522b999aeee09cedc8d5
marchdillo <kevinno.limiy@gmail.com>
2025-05-15 21:01:11 +0700

    commit 5

--- DoubleLinkedlist.cpp
+++ DoubleLinkedlist.cpp
@@ -20,4 +20,18 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+
+        // step 1: alocate memory for new node
+        node *newNode = new node();
+
+        // step 2: assign value to the data fields
+        newNode->noMhs = nim;
+    }
 };
