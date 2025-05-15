6302e8df56f900d4997ddf3d22729e4e2531840d
marchdillo <kevinno.limiy@gmail.com>
2025-05-15 21:18:02 +0700

    commit 9

--- DoubleLinkedlist.cpp
+++ DoubleLinkedlist.cpp
@@ -63,5 +63,10 @@ public:
         {
             current = current->next;
         }
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
     }
 };
